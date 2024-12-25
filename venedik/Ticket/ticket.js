e84de715ea1d1da823a5fd18db6843f3062cd657
semaabdullazada <abdsema0@gmail.com>
2024-12-25 15:26:35 +0400

    7-ci commit

---
+++ venedik/Ticket/ticket.js
@@ -0,0 +1,33 @@
+const eventSelect = document.getElementById('event');
+const adultSelect = document.getElementById('adult');
+const childSelect = document.getElementById('child');
+const totalPrice = document.getElementById('total-price');
+
+function calculatePrice() {
+    // Qiymət parametrləri
+    const prices = {
+        rio: { adult: 240, child: 40 },
+        holi: { adult: 240, child: 90 },
+        karaoke: { adult: 400, child: 50 },
+        venice: { adult: 400, child: 40 },
+        holi: { adult: 150, child: 90 },
+        skydiving: { adult: 120, child: 70 }
+    };
+
+    const selectedEvent = eventSelect.value;
+    const adultPrice = prices[selectedEvent].adult;
+    const childPrice = prices[selectedEvent].child;
+
+    const adults = parseInt(adultSelect.value);
+    const children = parseInt(childSelect.value);
+
+    const total = (adults * adultPrice) + (children * childPrice);
+    totalPrice.textContent =   `$${total}`;
+}
+
+eventSelect.addEventListener('change', calculatePrice);
+adultSelect.addEventListener('change', calculatePrice);
+childSelect.addEventListener('change', calculatePrice);
+
+// İlk hesablamanı çağır
+calculatePrice();
